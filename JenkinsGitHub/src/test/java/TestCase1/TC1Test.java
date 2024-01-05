541399973725c25011bc598233dec69c78369bce
UAER <UAER@LAPTOP-BM3QUGGE.mshome.net>
2024-01-05 12:40:27 +0530

    Eng1 first commit

---
+++ JenkinsGitHub/src/test/java/TestCase1/TC1Test.java
@@ -0,0 +1,13 @@
+package TestCase1;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TC1Test {
+	@Test
+	public void m1()
+	{
+		Reporter.log("M1 executed",true);
+	}
+
+}

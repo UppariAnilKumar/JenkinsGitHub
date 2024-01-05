52938d8ffdb40c0d087feccadf132dc763a966ab
UAER <UAER@LAPTOP-BM3QUGGE.mshome.net>
2024-01-05 18:28:21 +0530

    Eng1 2nd Commit

---
+++ JenkinsGitHub/src/test/java/TestCase2/DemoTest.java
@@ -0,0 +1,13 @@
+package TestCase2;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	@Test
+	public void k1()
+	{
+		Reporter.log("K1 pass",true);
+	}
+
+}

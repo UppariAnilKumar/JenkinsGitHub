443181f76a2cba3938f6ea73e3841690c94ac5dd
UAER <UAER@LAPTOP-BM3QUGGE.mshome.net>
2024-01-05 12:55:51 +0530

    Eng2 First commit

---
+++ JenkinsGitHub/src/test/java/TestCase1/TestCase2.java
@@ -0,0 +1,13 @@
+package TestCase1;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestCase2 {
+	@Test
+	public void m2()
+	{
+		Reporter.log("m2 executed",true);
+	}
+
+}

2627958db345d29663bf90517e847cf7b64e2456
UAER <UAER@LAPTOP-BM3QUGGE.mshome.net>
2024-01-06 12:09:01 +0530

    Eng1 3rd commit

---
+++ JenkinsGitHub/src/test/java/TestCase1/Tc3Test.java
@@ -0,0 +1,13 @@
+package TestCase1;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Tc3Test {
+	@Test
+	public void p1()
+	{
+		Reporter.log("p1 tested",true);
+	}
+
+}

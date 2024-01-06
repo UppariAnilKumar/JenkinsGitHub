adc0a4c84319fa16482cee54ec220d64e490b02c
UAER <UAER@LAPTOP-BM3QUGGE.mshome.net>
2024-01-06 13:00:13 +0530

    tc3Test Update

--- JenkinsGitHub/src/test/java/TestCase1/Tc3Test.java
+++ JenkinsGitHub/src/test/java/TestCase1/Tc3Test.java
@@ -8,6 +8,9 @@ public class Tc3Test {
 	public void p1()
 	{
 		Reporter.log("p1 tested",true);
+		Reporter.log("p1 tested",true);
+		Reporter.log("p1 tested",true);
+		Reporter.log("p1 tested",true);
 	}
 
 }
